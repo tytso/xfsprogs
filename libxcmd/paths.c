54ca4ea3efd69304ef413d958c0eeee660060eb2
Jan Tulak <jtulak@redhat.com>
2015-08-18 17:53:17 +1000

    xfsprogs: undefined variable fix
    Typo fix, which wasn't catch earlier due to #ifdef branching.  The
    'rmnttomname' does not exists anywhere and looks like a hybrid between
    rmntfromname and rmntonname. And because the previous if has has
    'fromname' on both arguments of realpath, I choose the same approach
    when fixing it.
    
    Signed-off-by: Jan Tulak <jtulak@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dave Chinner <david@fromorbit.com>

--- libxcmd/paths.c
+++ libxcmd/paths.c
@@ -364,7 +364,7 @@ fs_table_initialise_mounts(
 			continue;
 		if (!realpath(stats[i].f_mntfromname, rmntfromname))
 			continue;
-		if (!realpath(stats[i].f_mntonname, rmnttomname)))
+		if (!realpath(stats[i].f_mntonname, rmntonname))
 			continue;
 
 		if (path &&
