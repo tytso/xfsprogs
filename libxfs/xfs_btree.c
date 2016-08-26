574b41535bd2b2a07b5ffdf466a6bb4585907f3e
Darrick J. Wong <darrick.wong@oracle.com>
2016-08-26 11:16:41 +1000

    xfs: don't perform lookups on zero-height btrees
    If the caller passes in a cursor to a zero-height btree (which is
    impossible), we never set block to anything but NULL, which causes the
    later dereference of it to crash.  Instead, just return -EFSCORRUPTED.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dave Chinner <david@fromorbit.com>

--- libxfs/xfs_btree.c
+++ libxfs/xfs_btree.c
@@ -1810,6 +1810,10 @@ xfs_btree_lookup(
 
 	XFS_BTREE_STATS_INC(cur, lookup);
 
+	/* No such thing as a zero-level tree. */
+	if (cur->bc_nlevels == 0)
+		return -EFSCORRUPTED;
+
 	block = NULL;
 	keyno = 0;
 
