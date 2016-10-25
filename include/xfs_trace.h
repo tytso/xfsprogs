34621a4761b8314d4bbba1507d55d73e731c6e24
Darrick J. Wong <darrick.wong@oracle.com>
2016-10-25 12:31:37 +1100

    xfs: map an inode's offset to an exact physical block
    Source kernel commit: f65306ea5246ef3ff68a6abf85f5a73a04903366
    
    Teach the bmap routine to know how to map a range of file blocks to a
    specific range of physical blocks, instead of simply allocating fresh
    blocks.  This enables reflink to map a file to blocks that are already
    in use.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -271,6 +271,9 @@
 #define trace_xfs_refcount_find_shared_result(...)	((void) 0)
 #define trace_xfs_refcount_find_shared_error(...)	((void) 0)
 
+#define trace_xfs_bmap_remap_alloc(...)		((void) 0)
+#define trace_xfs_bmap_remap_alloc_error(...)	((void) 0)
+
 /* set c = c to avoid unused var warnings */
 #define trace_xfs_perag_get(a,b,c,d)		((c) = (c))
 #define trace_xfs_perag_get_tag(a,b,c,d)	((c) = (c))
