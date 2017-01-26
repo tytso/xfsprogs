3297e0caa25a235d2351ce3e1a90b8425242528d
Amir Goldstein <amir73il@gmail.com>
2017-01-25 20:02:41 -0600

    xfs: replace xfs_mode_to_ftype table with switch statement
    Source kernel commit: 1fc4d33fed124fb182e8e6c214e973a29389ae83
    
    The size of the xfs_mode_to_ftype[] conversion table
    was too small to handle an invalid value of mode=S_IFMT.
    
    Instead of fixing the table size, replace the conversion table
    with a conversion helper that uses a switch statement.
    
    Suggested-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- repair/phase6.c
+++ repair/phase6.c
@@ -1078,7 +1078,7 @@ mv_orphanage(
 	if ((err = -libxfs_iget(mp, NULL, ino, 0, &ino_p)))
 		do_error(_("%d - couldn't iget disconnected inode\n"), err);
 
-	xname.type = xfs_mode_to_ftype[(VFS_I(ino_p)->i_mode & S_IFMT)>>S_SHIFT];
+	xname.type = xfs_mode_to_ftype(VFS_I(ino_p)->i_mode);
 
 	if (isa_dir)  {
 		irec = find_inode_rec(mp, XFS_INO_TO_AGNO(mp, orphanage_ino),
